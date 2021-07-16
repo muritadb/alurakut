51c9b1289bcd12011bb0ab664f2beeffd9dbf521
muritadb <douglasb008@gmail.com>
2021-07-15 22:45:50 -0300

    followers working

--- pages/index.js
+++ pages/index.js
@@ -23,11 +23,45 @@ function ProfileSidebar(props) {
   )
 }
 
+function ProfileRelationsBox(props) {
+  return (
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        {props.title} ({props.items.length})
+      </h2>
+
+      <ul>
+        {/* {seguidores.map((itemAtual) => {
+        return (
+          <li key={itemAtual.id}>
+            <a href={`https://github.com/${itemAtual}.png`} >
+              <img src={itemAtual.image} />
+              <span>{itemAtual.title}</span>
+            </a>
+          </li>
+        )
+      })}  */}
+      </ul>
+    </ProfileRelationsBoxWrapper>
+  )
+}
+
 export default function Home() {
   const usuarioAleatorio = 'muritadb';
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = ['jaydson', 'omariosouto', 'willianjusten', 'felipefialho', 'anabastos', 'peas']
 
+  const [seguidores, setSeguidores] = React.useState([]);
+
+  React.useEffect(function () {
+    fetch('https://api.github.com/users/muritadb/followers')
+      .then(function (respostaDoServidor) {
+        return respostaDoServidor.json();
+      })
+      .then(function (respostaCompleta) {
+        setSeguidores(respostaCompleta);
+      })
+  }, [])
 
   return (
     <>
@@ -84,11 +118,11 @@ export default function Home() {
         </div>
 
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBox title="Seguidores" items={seguidores} />
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Comunidades({comunidades.length})
             </h2>
-
             <ul>
               {comunidades.map((itemAtual) => {
                 return (
